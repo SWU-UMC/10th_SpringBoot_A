52228ffe5bed8d8c91264b5b5007ed434d2a4414
SungMinju <minju2928@naver.com>
2026-05-05 22:49:33 +0900

    [Feat] 5주차 미션(유저코드 추가)

--- mintori/Umc10th/src/main/java/com/example/umc10th/domain/user/converter/UserConverter.java
+++ mintori/Umc10th/src/main/java/com/example/umc10th/domain/user/converter/UserConverter.java
@@ -1,4 +1,26 @@
 package com.example.umc10th.domain.user.converter;
 
+import com.example.umc10th.domain.user.dto.request.UserRequest;
+import com.example.umc10th.domain.user.dto.response.UserResponse;
+import com.example.umc10th.domain.user.entity.User;
+
 public class UserConverter {
+
+    private UserConverter() {
+    }
+
+    public static User toUser(UserRequest request) {
+        return User.builder()
+                .name(request.name())
+                .email(request.email())
+                .password(request.password())
+                .gender(request.gender())
+                .address(request.address())
+                .point(0L)
+                .build();
+    }
+
+    public static UserResponse toUserResponse(User user) {
+        return new UserResponse(user.getId(), user.getCreatedAt());
+    }
 }
