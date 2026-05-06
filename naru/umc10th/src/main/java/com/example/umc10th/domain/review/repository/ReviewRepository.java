b75ebe3f940b45256afb57505d182bb1464158a6
yangjiae12 <yangjiae12@gmail.com>
2026-05-06 16:09:19 +0900

    feat: 리뷰 작성 API 구현 #17

---
+++ naru/umc10th/src/main/java/com/example/umc10th/domain/review/repository/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.example.umc10th.domain.review.repository;
+
+import com.example.umc10th.domain.review.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    boolean existsByUserMissionId(Long userMissionId);
+}
